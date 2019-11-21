2c42c35ef7dbefe5e164a35fdf6141dae4a5dbe8
Li <Neal.Li@gov.bc.ca>
2019-11-21 11:18:53 -0800

    Add UAT to deploy configs

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -7,8 +7,9 @@ const name = 'hmcr'
 const phases = {
   build:  {namespace:'txkggj-tools' , name: `${name}`, phase: 'build' , changeId:changeId, suffix: `-build-${changeId}` , instance: `${name}-build-${changeId}` , version:`${version}-${changeId}`  , tag:`build-${version}-${changeId}`},
   dev:    {namespace:'txkggj-dev'   , name: `${name}`, phase: 'dev'   , changeId:changeId, suffix: `-dev-${changeId}`   , instance: `${name}-dev-${changeId}`   , version:`${version}-${changeId}`  , tag:`dev-${version}-${changeId}`  , host: `hmr-${changeId}-txkggj-dev.pathfinder.gov.bc.ca` , dotnet_env: 'Development'},
-  test:   {namespace:'txkggj-test'  , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`              , instance: `${name}-test`              , version:`${version}`              , tag:`test-${version}`             , host: `hmr-txkggj-test.pathfinder.gov.bc.ca`            , dotnet_env: 'Production'},
-  prod:   {namespace:'txkggj-prod'  , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`              , instance: `${name}-prod`              , version:`${version}`              , tag:`prod-${version}`             , host: `hmr-txkggj-test.pathfinder.gov.bc.ca`            , dotnet_env: 'Production'},
+  test:   {namespace:'txkggj-test'  , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`              , instance: `${name}-test`              , version:`${version}`              , tag:`test-${version}`             , host: `hmr-txkggj-test.pathfinder.gov.bc.ca`            , dotnet_env: 'Test'},
+  test:   {namespace:'txkggj-test'  , name: `${name}`, phase: 'uat'  ,  changeId:changeId, suffix: `-uat`              ,  instance: `${name}-uat`              ,  version:`${version}`              , tag:`uat-${version}`             ,  host: `hmr-txkggj-uat.pathfinder.gov.bc.ca`            ,  dotnet_env: 'UAT'},
+  prod:   {namespace:'txkggj-prod'  , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`              , instance: `${name}-prod`              , version:`${version}`              , tag:`prod-${version}`             , host: `hmr-txkggj.pathfinder.gov.bc.ca`            ,      dotnet_env: 'Production'},
 };
 
 // This callback forces the node process to exit as failure.
