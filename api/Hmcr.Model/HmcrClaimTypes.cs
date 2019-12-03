0a760c0ba99cf62c59e89bdcebe661c2c45d0fcb
Young-Jin Chung <young-jin.chung@gov.bc.ca>
2019-12-03 09:54:30 -0800

    Reverted Keycloak stuff

--- api/Hmcr.Model/HmcrClaimTypes.cs
+++ api/Hmcr.Model/HmcrClaimTypes.cs
@@ -12,11 +12,11 @@
         public const string UniversalId = "SM_UNIVERSALID";
         public const string AuthDirName = "SM_AUTHDIRNAME";
 
-        public const string KcUsername = "preferred_username";
-        public const string KcUserGuid = "user_guid";
-        public const string KcBusinessGuid = "business_guid";
-        public const string KcBusinessLegalName = "business_legal_name";
-        public const string KcBusinessNumber = "business_number";
+        //public const string KcUsername = "preferred_username";
+        //public const string KcUserGuid = "user_guid";
+        //public const string KcBusinessGuid = "business_guid";
+        //public const string KcBusinessLegalName = "business_legal_name";
+        //public const string KcBusinessNumber = "business_number";
 
         public const string Permission = "HMCR_PERMISSION";
         public const string ServiceAreaNumber = "HMCR_SERVICE_AREA_NUMBER";
