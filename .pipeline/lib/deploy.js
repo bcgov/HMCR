9db5d295275bad7180d6a34972a8d06fb2dd97d3
Young-Jin Chung <young-jin.chung@gov.bc.ca>
2020-01-08 11:42:17 -0800

    ENV variable to deploy pipeline

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -1,15 +1,19 @@
-'use strict';
-const {OpenShiftClientX} = require('pipeline-cli')
-const path = require('path');
+"use strict";
+const { OpenShiftClientX } = require("pipeline-cli");
+const path = require("path");
 
-module.exports = (settings)=>{
-  const phases = settings.phases
-  const options= settings.options
-  const phase=options.env
-  const changeId = phases[phase].changeId
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
-  var objects = []
+module.exports = settings => {
+  const phases = settings.phases;
+  const options = settings.options;
+  const phase = options.env;
+  const changeId = phases[phase].changeId;
+  const oc = new OpenShiftClientX(
+    Object.assign({ namespace: phases[phase].namespace }, options)
+  );
+  const templatesLocalBaseUrl = oc.toFileUrl(
+    path.resolve(__dirname, "../../openshift")
+  );
+  var objects = [];
 
   // The deployment of your cool app goes here ▼▼▼
   objects.push(
@@ -20,6 +24,7 @@ module.exports = (settings)=>{
           NAME: `${phases[phase].name}-client`,
           SUFFIX: phases[phase].suffix,
           VERSION: phases[phase].tag,
+          ENV: phases[phase].phase,
           HOST: phases[phase].host
         }
       }
@@ -42,7 +47,18 @@ module.exports = (settings)=>{
     )
   );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
-  oc.applyAndDeploy(objects, phases[phase].instance)
-}
+  oc.applyRecommendedLabels(
+    objects,
+    phases[phase].name,
+    phase,
+    `${changeId}`,
+    phases[phase].instance
+  );
+  oc.importImageStreams(
+    objects,
+    phases[phase].tag,
+    phases.build.namespace,
+    phases.build.tag
+  );
+  oc.applyAndDeploy(objects, phases[phase].instance);
+};
