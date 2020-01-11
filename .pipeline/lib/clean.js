536ce711a057897ee93e4c7f940ca48fb21dd636
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-01-10 23:19:28 -0800

    Clean script clean up... (#105)

--- .pipeline/lib/clean.js
+++ .pipeline/lib/clean.js
@@ -23,33 +23,32 @@ module.exports = (settings)=>{
   const target_phases=getTargetPhases(options.env, phases);
   const kc = new KeyCloakClient(settings, oc);  
 
-  for (var k in phases){
+  target_phases.forEach(k => {
     if (phases.hasOwnProperty(k)) {
       const phase=phases[k]
-      if (target_phases.includes(k) ){
 
-        if(k === 'dev')
-          kc.remmoveUris();
-        
-        let buildConfigs=oc.get('bc', {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, namespace:phase.namespace})
-        buildConfigs.forEach((bc)=>{
-          if (bc.spec.output.to.kind == 'ImageStreamTag'){
-            oc.delete([`ImageStreamTag/${bc.spec.output.to.name}`],{'ignore-not-found':'true', 'wait':'true', namespace:phase.namespace})
+      if(k === 'dev')
+        kc.remmoveUris();
+      
+      let buildConfigs=oc.get('bc', {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, namespace:phase.namespace})
+      buildConfigs.forEach((bc)=>{
+        if (bc.spec.output.to.kind == 'ImageStreamTag'){
+          oc.delete([`ImageStreamTag/${bc.spec.output.to.name}`],{'ignore-not-found':'true', 'wait':'true', namespace:phase.namespace})
+        }
+      })
+      
+      let deploymentConfigs=oc.get('dc', {selector:`app=${phase.instance},env-id=${phase.changeId},env-name=${k},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, namespace:phase.namespace})
+      deploymentConfigs.forEach((dc)=>{
+        dc.spec.triggers.forEach((trigger)=>{
+          if (trigger.type == 'ImageChange' && trigger.imageChangeParams.from.kind == 'ImageStreamTag'){
+            oc.delete([`ImageStreamTag/${trigger.imageChangeParams.from.name}`],{'ignore-not-found':'true', 'wait':'true', namespace:phase.namespace})
           }
         })
-        
-        let deploymentConfigs=oc.get('dc', {selector:`app=${phase.instance},env-id=${phase.changeId},env-name=${k},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, namespace:phase.namespace})
-        deploymentConfigs.forEach((dc)=>{
-          dc.spec.triggers.forEach((trigger)=>{
-            if (trigger.type == 'ImageChange' && trigger.imageChangeParams.from.kind == 'ImageStreamTag'){
-              oc.delete([`ImageStreamTag/${trigger.imageChangeParams.from.name}`],{'ignore-not-found':'true', 'wait':'true', namespace:phase.namespace})
-            }
-          })
-        })
-        
-        oc.raw('delete', ['all'], {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, wait:'true', namespace:phase.namespace})
-        oc.raw('delete', ['pvc,Secret,configmap,endpoints,RoleBinding,role,ServiceAccount,Endpoints'], {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, wait:'true', namespace:phase.namespace})
-      }
+      })
+      
+      oc.raw('delete', ['all'], {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, wait:'true', namespace:phase.namespace})
+      oc.raw('delete', ['pvc,Secret,configmap,endpoints,RoleBinding,role,ServiceAccount,Endpoints'], {selector:`app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, wait:'true', namespace:phase.namespace})
+      
     }
-  }
+  });
 }
