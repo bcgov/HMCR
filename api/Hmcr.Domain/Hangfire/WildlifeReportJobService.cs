70d52966a52de4d9e22fc2b8ffd2327adab626c8
Young-Jin Chung <44654482+ychung-mot@users.noreply.github.com>
2020-01-23 10:25:09 -0800

    More logging and suppressing Hangifre connection timeout failure.

--- api/Hmcr.Domain/Hangfire/WildlifeReportJobService.cs
+++ api/Hmcr.Domain/Hangfire/WildlifeReportJobService.cs
@@ -119,7 +119,7 @@ namespace Hmcr.Domain.Hangfire
 
                 await _unitOfWork.CommitAsync();
 
-                _logger.LogInformation("Report Saved");
+                _logger.LogInformation($"[Hangfire] Submission {submission.SubmissionObjectId} processed successfully.");
             }
 
             _logger.LogInformation("[Hangfire] Finishing submission {submissionObjectId}", submission.SubmissionObjectId);
