93d1337ba53fdc1849acf9e2cac9fc3f8111d3af
Li <Neal.Li@gov.bc.ca>
2020-01-23 13:27:40 -0800

    Client one docker image WIP

---
+++ client/nginx-start/env2js.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+JS_PATH=~/js/env.config.js
+
+echo "window.RUNTIME_REACT_APP_SSO_HOST='${REACT_APP_SSO_HOST}';" > $JS_PATH
+echo "window.RUNTIME_REACT_APP_SSO_REALM='${REACT_APP_SSO_REALM}';" >> $JS_PATH
+echo "window.RUNTIME_REACT_APP_SSO_CLIENT='${REACT_APP_SSO_CLIENT}';" >> $JS_PATH
+echo "window.RUNTIME_REACT_APP_API_HOST='${REACT_APP_API_HOST}';" >> $JS_PATH
