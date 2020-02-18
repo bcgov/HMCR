1e1ef31988d40ec2b251332e081cb61724849811
Li <Neal.Li@gov.bc.ca>
2020-02-18 08:53:57 -0800

    Remove unused redux code

--- client/src/js/actions/index.js
+++ client/src/js/actions/index.js
@@ -1,6 +1,4 @@
 export * from './codeLookupsActions';
 export * from './errorDialogActions';
-export * from './roleActions';
 export * from './serviceAreaActions';
-export * from './submissionsActions';
 export * from './userActions';
