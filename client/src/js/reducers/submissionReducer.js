217b7c47051ab1a17b5ecd4b3667fa223e9db651
Li <Neal.Li@gov.bc.ca>
2020-03-09 09:14:37 -0700

    Add submission streams to redux

--- client/src/js/reducers/submissionReducer.js
+++ client/src/js/reducers/submissionReducer.js
@@ -1,15 +1,18 @@
 import _ from 'lodash';
 
-import { FETCH_SUBMISSION_STATUSES } from '../actions/types';
+import { FETCH_SUBMISSION_STATUSES, FETCH_SUBMISSION_STREAMS } from '../actions/types';
 
 const defaultState = {
   statuses: {},
+  streams: {},
 };
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case FETCH_SUBMISSION_STATUSES:
       return { ...state, statuses: { ..._.mapKeys(action.payload, 'statusCode') } };
+    case FETCH_SUBMISSION_STREAMS:
+      return { ...state, streams: action.payload };
     default:
       return state;
   }
