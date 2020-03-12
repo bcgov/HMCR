bb84d4a6442b37a8b27792a1cff93e4e0cc4cdb2
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-03-12 14:06:41 -0700

    HMCR-556 Limit upload file name length to 100 on the UI (#303)

--- client/src/js/components/WorkReportingUpload.js
+++ client/src/js/components/WorkReportingUpload.js
@@ -185,6 +185,15 @@ const WorkReportingUpload = ({
 
   const validateFile = (e, setFieldValue, setFieldError, fieldName, sizeLimit) => {
     const file = e.currentTarget.files[0];
+
+    console.log(file);
+    console.log(file.name.length);
+
+    if (file.name.length > 100) {
+      setFieldError(fieldName, 'The file name exceeds the maximum allowed length of 100 characters.');
+      return;
+    }
+
     if (!isFileCsvType(file)) {
       setFieldError(fieldName, 'The selected file is not a CSV file');
       return;
