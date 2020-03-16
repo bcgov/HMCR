2120afe7bf89dc72828844cfd3610c8298e6356f
Young-Jin Chung <44654482+ychung-mot@users.noreply.github.com>
2020-03-16 14:03:31 -0700

    Separating Hangfire server from Api

---
+++ api/Hmcr.Hangfire/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Hmcr.Hangfire
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration))
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                });
+    }
+}
--- api/Hmcr.Hangfire/Startup.cs
+++ api/Hmcr.Hangfire/Startup.cs
@@ -1,3 +1,4 @@
+using Hangfire;
 using Hmcr.Api.Extensions;
 using Hmcr.Bceid;
 using Hmcr.Chris;
@@ -10,11 +11,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Hmcr.Api
+namespace Hmcr.Hangfire
 {
     public class Startup
     {
-
         public IConfiguration Configuration { get; }
 
         private IWebHostEnvironment _env;
@@ -28,37 +28,43 @@ namespace Hmcr.Api
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration.GetValue<string>("ConnectionStrings:HMCR");
+            var runHangfireServer = Configuration.GetValue<bool>("Hangfire:EnableServer");
+            var workerCount = Configuration.GetValue<int>("Hangfire:WorkerCount");
 
             services.AddHttpContextAccessor();
-            services.AddHmcrAuthentication(Configuration);
             services.AddHmcrDbContext(connectionString);
-            services.AddCors();
-            services.AddHmcrControllers();
             services.AddHmcrAutoMapper();
-            services.AddHmcrApiVersioning();
             services.AddHmcrTypes();
-            services.AddHmcrSwagger(_env);
             services.AddChrisHttpClient(Configuration);
             services.AddBceidSoapClient(Configuration);
+            services.AddHmcrHangfire(connectionString, runHangfireServer, workerCount);
             services.AddHmcrHealthCheck(connectionString);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ISubmissionObjectJobService jobService, 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ISubmissionObjectJobService jobService,
             IServiceScopeFactory serviceScopeFactory, IServiceAreaService svcAreaService, ICodeLookupRepository codeLookupRepo, IFieldValidatorService validator)
         {
             if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();            
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseHttpsRedirection();
-            app.UseHmcrCors();
             app.UseExceptionMiddleware();
             app.UseHmcrHealthCheck();
-            app.UseAuthorization();
-            app.UseAuthentication();
-            app.UseRouting();
-            app.UseHmcrEndpoints();
-            app.UseHmcrSwagger(env, Configuration.GetSection("Constants:SwaggerApiUrl").Value);
+            app.UseHangfireDashboard();
 
+            //Register Hangfire Recurring Jobs 
+            var serviceAreas = svcAreaService.GetAllServiceAreas();
+
+            var minutes = Configuration.GetValue<int>("Hangfire:ReportJobIntervalInMinutes");
+            minutes = minutes < 1 ? 5 : minutes;
+
+            foreach (var serviceArea in serviceAreas)
+            {
+                RecurringJob.AddOrUpdate<SubmissionObjectJobService>($"SA{serviceArea.ServiceAreaNumber}", x => x.RunReportingJob(serviceArea.ServiceAreaNumber), $"*/{minutes} * * * *");
+            }
+
+            //Inject Code Lookup
             validator.CodeLookup = codeLookupRepo.LoadCodeLookupCache();
         }
     }
