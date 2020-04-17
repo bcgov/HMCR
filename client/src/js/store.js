fe7ac4b9ad9a6a76be37753f5a92f4d23e793b54
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-04-17 15:31:13 -0700

    Disable redux-devtools in production (#424)

--- client/src/js/store.js
+++ client/src/js/store.js
@@ -1,15 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middleware =
   process.env.NODE_ENV !== 'production'
     ? [require('redux-immutable-state-invariant').default(), reduxThunk]
     : [reduxThunk];
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
