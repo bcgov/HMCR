6523357e988a34b3cf9bcb265b1c812a1ccf7bd7
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-04-17 23:01:01 -0700

    Add KeyCloak User Password API

---
+++ api/Hmcr.Model/KeyCloakHttp.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hmcr.Model
+{
+    public class KeyCloakTokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+    }
+
+    public class KeyCloakUserResponse
+    {
+        [JsonPropertyName("id")]
+        public string UserId { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class KeyCloakUserPasswordResetRequest
+    {
+        public string Type => "password";
+        public string Value { get; set; }
+        public bool Temporary => false;
+
+        public KeyCloakUserPasswordResetRequest(string value)
+        {
+            Value = value;
+        }
+    }
+}
