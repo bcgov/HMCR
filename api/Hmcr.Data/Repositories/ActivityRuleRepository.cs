8716a0d532af668df719b6cfc4f4b2ab4e6bd99e
Darrel Siegle <darrel.siegle@dxcas.com>
2020-10-05 15:00:47 -0700

    Created model to entity relationships Updated Repository classes for Activity Code/Rule

--- api/Hmcr.Data/Repositories/ActivityRuleRepository.cs
+++ api/Hmcr.Data/Repositories/ActivityRuleRepository.cs
@@ -18,7 +18,7 @@ namespace Hmcr.Data.Repositories
         Task<IEnumerable<ActivityRuleDto>> GetRoadClassRulesAsync();
     }
 
-    public class ActivityRuleRepository : HmcrRepositoryBase<HmrServiceArea>, IActivityRuleRepository //TODO
+    public class ActivityRuleRepository : HmcrRepositoryBase<HmrActivityRule>, IActivityRuleRepository
     {
         public ActivityRuleRepository(AppDbContext dbContext, IMapper mapper)
             : base(dbContext, mapper)
@@ -27,58 +27,57 @@ namespace Hmcr.Data.Repositories
 
         public async Task<IEnumerable<ActivityRuleDto>> GetRoadLengthRulesAsync()
         {
-            //TODO
-            //var activityRules = await DbSet.AsNoTracking()
-            //    .Where(s => s.HmrActivityRules.Any(sa => sa.TYPE == "RoadLengthRules"))
-            //    .ToListAsync();
+            var activityRules = await DbSet.AsNoTracking()
+                .Where(s => s.ActivityRuleSet.ToUpper() == "ROAD_LENGTH")
+                .ToListAsync();
 
-            //return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
+            return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
 
             //fake data
-            ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
+            /*ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
             {
                 new ActivityRuleDto{ ActivityRuleId = 1, ActivityRuleName = "Road Length Rule 1" },
                 new ActivityRuleDto{ ActivityRuleId = 2, ActivityRuleName = "Road Length Rule 2" },
                 new ActivityRuleDto{ ActivityRuleId = 3, ActivityRuleName = "Road Length Rule 3" }
             };
             await Task.Delay(1);
-            return activityRules;
+            return activityRules;*/
         }
         public async Task<IEnumerable<ActivityRuleDto>> GetSurfaceTypeRulesAsync()
         {
-            //var activityRules = await DbSet.AsNoTracking()
-            //    .Where(s => s.HmrActivityRules.Any(sa => sa.TYPE == "SurfaceTypeRules"))
-            //    .ToListAsync();
+            var activityRules = await DbSet.AsNoTracking()
+                .Where(s => s.ActivityRuleSet.ToUpper() == "SURFACE_TYPE")
+                .ToListAsync();
 
-            //return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
+            return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
 
             //fake data
-            ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
+            /*ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
             {
                 new ActivityRuleDto{ ActivityRuleId = 1, ActivityRuleName = "Surface Type Rule 1" },
                 new ActivityRuleDto{ ActivityRuleId = 2, ActivityRuleName = "Surface Type Rule 2" },
                 new ActivityRuleDto{ ActivityRuleId = 3, ActivityRuleName = "Surface Type Rule 3" }
             };
             await Task.Delay(1);
-            return activityRules;
+            return activityRules;*/
         }
         public async Task<IEnumerable<ActivityRuleDto>> GetRoadClassRulesAsync()
         {
-            //var activityRules = await DbSet.AsNoTracking()
-            //    .Where(s => s.HmrActivityRules.Any(sa => sa.TYPE == "RoadClass"))
-            //    .ToListAsync();
+            var activityRules = await DbSet.AsNoTracking()
+                .Where(s => s.ActivityRuleSet.ToUpper() == "ROAD_CLASS")
+                .ToListAsync();
 
-            //return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
+            return Mapper.Map<IEnumerable<ActivityRuleDto>>(activityRules);
 
             //fake data
-            ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
+            /*ActivityRuleDto[] activityRules = new ActivityRuleDto[3]
             {
                 new ActivityRuleDto{ ActivityRuleId = 1, ActivityRuleName = "Road Class Rule 1" },
                 new ActivityRuleDto{ ActivityRuleId = 2, ActivityRuleName = "Road Class Rule 2" },
                 new ActivityRuleDto{ ActivityRuleId = 3, ActivityRuleName = "Road Class Rule 3" }
             };
             await Task.Delay(1);
-            return activityRules;
+            return activityRules;*/
         }
     }
 }
