3801619a9dae91b237b3e3cc449743c074e6e99c
Young-Jin Chung <Young-Jin.Chung@gov.bc.ca>
2020-11-03 12:14:18 -0800

    Api URL

--- client/src/js/Constants.js
+++ client/src/js/Constants.js
@@ -1,6 +1,6 @@
 export const API_URL = window.RUNTIME_REACT_APP_API_HOST
   ? `${window.location.protocol}//${window.RUNTIME_REACT_APP_API_HOST}/api`
-  : process.env.REACT_APP_API_HOST;
+  : `https://api-${process.env.REACT_APP_API_HOST}/api`;
 
 const CODE_LOOKUP = '/codelookup';
 
