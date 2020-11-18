420c5ea4dec0b4914b539281f18d8014b0f568d5
AdvSol-Wilson <Wilson.Wang@dxcas.com>
2020-11-17 16:33:25 -0800

    IS2.4 DB changes

---
+++ database/V27.0/2_HMR_PDM-dml_RECORD_VERSION_NUMBER_UPD_V27_IS2.4.sql
@@ -0,0 +1,43 @@
+-- *****************************************************************************
+-- Script to initially load the RECORD_VERSION_NUMBER field from the contents of
+-- the CONCURRENCY_CONTROL_NUMBER in the following tables:
+-- * HMR_ROCKFALL_REPORT
+-- * HMR_ROCKFALL_REPORT_HIST
+-- * HMR_WORK_REPORT
+-- * HMR_WORK_REPORT_HIST
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Date         Author        Comment
+-- -----------  ------------  --------------------------------------------------
+-- 2020-Nov-16  Doug Filteau  Initial Version
+-- *****************************************************************************
+
+USE HMR_DEV
+GO
+
+-- *****************************************************************************
+-- Update the RECORD_VERSION_NUMBER column on the HMR_ROCKFALL_REPORT table
+-- *****************************************************************************
+UPDATE [dbo].[HMR_ROCKFALL_REPORT]
+SET    RECORD_VERSION_NUMBER = CONCURRENCY_CONTROL_NUMBER
+GO
+
+-- *****************************************************************************
+-- Update the RECORD_VERSION_NUMBER column on the HMR_ROCKFALL_REPORT_HIST table
+-- *****************************************************************************
+UPDATE [dbo].[HMR_ROCKFALL_REPORT_HIST]
+SET    RECORD_VERSION_NUMBER = CONCURRENCY_CONTROL_NUMBER
+GO
+
+-- *****************************************************************************
+-- Update the RECORD_VERSION_NUMBER column on the HMR_WORK_REPORT table
+-- *****************************************************************************
+UPDATE [dbo].[HMR_WORK_REPORT]
+SET    RECORD_VERSION_NUMBER = CONCURRENCY_CONTROL_NUMBER
+GO
+
+-- *****************************************************************************
+-- Update the RECORD_VERSION_NUMBER column on the HMR_WORK_REPORT_HIST table
+-- *****************************************************************************
+UPDATE [dbo].[HMR_WORK_REPORT_HIST]
+SET    RECORD_VERSION_NUMBER = CONCURRENCY_CONTROL_NUMBER
+GO
