918302b25bf3328d09cdbd25640e849292ac4b39
Darrel Siegle <darrel.siegle@dxcas.com>
2020-12-03 15:06:57 -0800

    HMCR-997

--- database/V27.0/8_HMR_PDM-dml_SUBMISSION_STATUS_INS_V27_IS2.4.sql
+++ database/V27.0/8_HMR_PDM-dml_SUBMISSION_STATUS_INS_V27_IS2.4.sql
@@ -9,7 +9,7 @@ INSERT INTO HMR_SUBMISSION_STATUS
 	APP_CREATE_USERID, APP_CREATE_TIMESTAMP, APP_CREATE_USER_GUID, APP_CREATE_USER_DIRECTORY,
 	APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USER_GUID, APP_LAST_UPDATE_USER_DIRECTORY)
 	VALUES 
-	('3P', 'Stage 3 – Validation in Progress', NULL, 'F', 3, 1, 
+	('3P', 'Stage 3 – Validation in Progress', 'Indicates a submitted file has been received and is being checked against expected submission requirements corresponding to the selected report type.', 'F', 3, 1, 
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH',
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH');
 GO
@@ -18,7 +18,7 @@ INSERT INTO HMR_SUBMISSION_STATUS
 	APP_CREATE_USERID, APP_CREATE_TIMESTAMP, APP_CREATE_USER_GUID, APP_CREATE_USER_DIRECTORY,
 	APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USER_GUID, APP_LAST_UPDATE_USER_DIRECTORY)
 	VALUES 
-	('4P', 'Stage 4 – Validation in Progress', NULL, 'F', 4, 1, 
+	('4P', 'Stage 4 – Validation in Progress', 'Indicates a submitted file has been received and is being checked against expected submission requirements corresponding to the selected report type.', 'F', 4, 1, 
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH',
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH');
 GO
@@ -36,7 +36,7 @@ INSERT INTO HMR_SUBMISSION_STATUS
 	APP_CREATE_USERID, APP_CREATE_TIMESTAMP, APP_CREATE_USER_GUID, APP_CREATE_USER_DIRECTORY,
 	APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USER_GUID, APP_LAST_UPDATE_USER_DIRECTORY)
 	VALUES 
-	('SW', 'Validation Successful with Warnings', NULL, 'F', 5, 1, 
+	('SW', 'Validation Successful with Warnings', 'Indicates a submitted file has been fully evaluated and the data meets submission requirements corresponding to the selected report type but also contains warnings based on system analysis of validation rules.', 'F', 5, 1, 
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH',
 	'sa', getutcdate(), '00000001-0000-0000-0000-000000000000', 'WIN AUTH');
 GO
