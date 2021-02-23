2eb52b9f4d51f188f5735936eb57c6b72356b69e
Darrel Siegle <darrel.siegle@dxcas.com>
2021-02-22 16:01:48 -0800

    adding logging for the Export URL

--- api/Hmcr.Chris/ExportApi.cs
+++ api/Hmcr.Chris/ExportApi.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace Hmcr.Chris
         private HttpClient _client;
         private IApi _api;
         private IConfiguration _config;
+        private ILogger<IExportApi> _logger;
 
         public ExportApi(HttpClient client, IApi api, IConfiguration config)
         {
@@ -24,6 +26,10 @@ namespace Hmcr.Chris
         public async Task<HttpResponseMessage> ExportReport(string query, string exportEndpointConfig)
         {
             string path = _config.GetValue<string>($"CHRIS:{exportEndpointConfig}");
+
+            //_client.BaseAddress.ToString() + path + "&" + query
+            _logger.LogInformation($"ExportReport - Calling Export URL { _client.BaseAddress.ToString() + path + "&" + query}");
+
             return await _api.Get(_client, path + "&" + query);
         }
     }
