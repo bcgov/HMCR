bf7bc5b2a5cd0f20d726d2173074c397420f2d40
Sean <Ionwyn.Sean@gov.bc.ca>
2024-01-04 10:31:41 -0800

    [TH-108510] Add default report DB values

--- api/Hmcr.Data/Database/Entities/HmrSaltReport.cs
+++ api/Hmcr.Data/Database/Entities/HmrSaltReport.cs
@@ -23,5 +23,19 @@ namespace Hmcr.Data.Database.Entities
         public string MaterialStorageAchieved { get; set; }
         public string SaltApplicationIdentified { get; set; }
         public string SaltApplicationAchieved { get; set; }
+        // Default
+        public long ConcurrencyControlNumber { get; set; }
+        public string AppCreateUserid { get; set; }
+        public DateTime AppCreateTimestamp { get; set; }
+        public Guid AppCreateUserGuid { get; set; }
+        public string AppCreateUserDirectory { get; set; }
+        public string AppLastUpdateUserid { get; set; }
+        public DateTime AppLastUpdateTimestamp { get; set; }
+        public Guid AppLastUpdateUserGuid { get; set; }
+        public string AppLastUpdateUserDirectory { get; set; }
+        public string DbAuditCreateUserid { get; set; }
+        public DateTime? DbAuditCreateTimestamp { get; set; }
+        public string DbAuditLastUpdateUserid { get; set; }
+        public DateTime? DbAuditLastUpdateTimestamp { get; set; }
     }
 }
