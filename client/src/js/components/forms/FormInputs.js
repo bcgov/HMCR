2a052348bfc98586e3a4c230f996cf88aa8f0d54
Sean <Ionwyn.Sean@gov.bc.ca>
2024-02-01 14:42:52 -0800

    [TH-108510] New number component
    Prevents HTML and e.target.value changing to string on empty inputs

--- client/src/js/components/forms/FormInputs.js
+++ client/src/js/components/forms/FormInputs.js
@@ -35,6 +35,34 @@ export const FormInput = ({ children, ...props }) => {
   );
 };
 
+export const FormNumberInput = ({ children, name, ...props }) => {
+  const [field, meta, helpers] = useField({ ...props, name, type: 'number' });
+  const { setValue } = helpers;
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    if (value === '') {
+      // Set the value to null if the input is cleared
+      setValue(null);
+    } else {
+      // Only parse the value as a number if it's not empty
+      value = props.type === 'number' ? parseFloat(value) : value;
+      // Check if parsing succeeded; otherwise, revert to original string
+      // This avoids setting the field to NaN for incomplete number inputs like "-"
+      setValue(!isNaN(value) ? value : e.target.value);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Input type="number" {...field} {...props} invalid={meta.error && meta.touched} onChange={handleChange}>
+        {children}
+      </Input>
+      {meta.error && meta.touched && <FormFeedback>{meta.error}</FormFeedback>}
+    </React.Fragment>
+  );
+};
+
 export const FormRadioInput = ({ label, ...props }) => {
   const [field, meta] = useField({ ...props, type: 'radio' });
   return (
