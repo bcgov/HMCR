9f4fe1ef2f445f4bb413504cfff75e0a391a114a
Ionwyn Sean <Ionwyn.Sean@gov.bc.ca>
2025-05-22 10:30:53 -0700

    Add linting rules

---
+++ client/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', 'import', 'jsx-a11y'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'react/prop-types': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
