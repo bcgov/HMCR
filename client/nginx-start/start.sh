8e7f961f6aef4767a7e7ebc16c6156217ad91173
Ionwyn Sean <Ionwyn.Sean@gov.bc.ca>
2025-05-28 12:44:25 -0700

    support Vite runtime env injection via env.config.js

--- client/nginx-start/start.sh
+++ client/nginx-start/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "---> Setting window.RUNTIME_REACT_APP values ..."
-JS_PATH=~/js/env.config.js
+JS_PATH=/opt/app-root/src/env.config.js
 
 echo "window.RUNTIME_REACT_APP_SSO_HOST='${VITE_SSO_HOST}';" > $JS_PATH
 echo "window.RUNTIME_REACT_APP_SSO_REALM='${VITE_SSO_REALM}';" >> $JS_PATH
@@ -10,3 +10,5 @@ echo "window.RUNTIME_REACT_APP_API_HOST='${VITE_API_HOST}';" >> $JS_PATH
 
 echo "---> Creating nginx.conf ..."
 envsubst '${HMCR_DEPLOY_SUFFIX}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
+
+exec "$@"
