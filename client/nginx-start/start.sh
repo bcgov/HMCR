1aa060635a7b0818e6f93b838159339c48bbd553
Ionwyn Sean <Ionwyn.Sean@gov.bc.ca>
2025-07-10 12:08:12 -0700

    Modify prefix

--- client/nginx-start/start.sh
+++ client/nginx-start/start.sh
@@ -9,6 +9,6 @@ echo "window.RUNTIME_REACT_APP_SSO_CLIENT='${VITE_SSO_CLIENT}';" >> $JS_PATH
 echo "window.RUNTIME_REACT_APP_API_HOST='${VITE_API_HOST}';" >> $JS_PATH
 
 echo "---> Creating nginx.conf ..."
-envsubst '${HMCR_DEPLOY_SUFFIX}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '${API_SERVICE_NAME}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
