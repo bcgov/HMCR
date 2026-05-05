968612ed9bc7f7309c3a1103a2501c15af1ecca4
Ionwyn Sean <ionwynsean@gmail.com>
2026-05-05 14:47:14 -0700

    [HMCR-1117] delete unused service worker
    The CRA-generated serviceWorker.js was only ever called via
    unregister(); it has never registered a worker. Removing the file and
    its import.

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import * as serviceWorker from './serviceWorker';
-
 /* eslint-disable import/first */
 const App = lazy(() => import('./js/App'));
 
@@ -20,8 +18,3 @@ Keycloak.init(() => {
     document.getElementById('root')
   );
 });
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
