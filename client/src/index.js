ac4f5e25031dad6c742aba1a8e3c41bd6ed3835e
Ionwyn Sean <ionwynsean@gmail.com>
2026-05-05 14:46:56 -0700

    [HMCR-1117] drop react-app-polyfill and IE11 support
    IE11 reached end-of-life in 2022. Removing the polyfill imports and the
    "ie 11" entry from browserslist.development reduces bundle weight and
    unblocks the move off CRA.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,3 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
