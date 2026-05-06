2d5e887489ab89561893b29735aab32de49bfa8f
Ionwyn Sean <ionwynsean@gmail.com>
2026-05-05 21:51:44 -0700

    [HMCR-1117] swap redux-devtools-extension for @redux-devtools/extension
    The original package was deprecated and explicitly redirected to
    @redux-devtools/extension (npm warned about this on every install).
    The /developmentOnly subpath and composeWithDevTools API are unchanged,
    so store.js needs only the import path updated.

--- client/src/js/store.js
+++ client/src/js/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { composeWithDevTools } from '@redux-devtools/extension/developmentOnly';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
