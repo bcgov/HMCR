f40665f42c920e567d0145ae1abe4421ac479c2e
Ionwyn Sean <ionwynsean@gmail.com>
2026-07-23 09:18:28 -0700

    Remove legacy OpenShift template deployment

--- .pipeline/build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const task = require('./lib/build.js')
-const settings = require('./lib/config.js')
-
-Promise.resolve(task(Object.assign(settings, { phase: 'build' }))).catch((error) => {
-  console.error(error);
-  process.exit(1);
-})
