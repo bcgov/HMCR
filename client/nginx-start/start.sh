0a7a3d23dbbe4cba3efb9410e906a377ca2dd3be
Ionwyn Sean <ionwynsean@gmail.com>
2026-07-23 09:18:33 -0700

    Add container builds for HMCR services

--- client/nginx-start/start.sh
+++ client/nginx-start/start.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+set -eu
 
 echo "---> Setting window.RUNTIME_REACT_APP values ..."
 JS_PATH=~/js/env.config.js
+mkdir -p "$(dirname "$JS_PATH")"
 
-echo "window.RUNTIME_REACT_APP_SSO_HOST='${REACT_APP_SSO_HOST}';" > $JS_PATH
-echo "window.RUNTIME_REACT_APP_SSO_REALM='${REACT_APP_SSO_REALM}';" >> $JS_PATH
-echo "window.RUNTIME_REACT_APP_SSO_CLIENT='${REACT_APP_SSO_CLIENT}';" >> $JS_PATH
-echo "window.RUNTIME_REACT_APP_API_HOST='${REACT_APP_API_HOST}';" >> $JS_PATH
-echo "window.RUNTIME_REACT_APP_VERSION='${REACT_APP_VERSION}';" >> $JS_PATH
+echo "window.RUNTIME_REACT_APP_SSO_HOST='${REACT_APP_SSO_HOST:-}';" > "$JS_PATH"
+echo "window.RUNTIME_REACT_APP_SSO_REALM='${REACT_APP_SSO_REALM:-}';" >> "$JS_PATH"
+echo "window.RUNTIME_REACT_APP_SSO_CLIENT='${REACT_APP_SSO_CLIENT:-}';" >> "$JS_PATH"
+echo "window.RUNTIME_REACT_APP_API_HOST='${REACT_APP_API_HOST:-}';" >> "$JS_PATH"
+echo "window.RUNTIME_REACT_APP_VERSION='${REACT_APP_VERSION:-}';" >> "$JS_PATH"
 
 echo "---> Creating nginx.conf ..."
 envsubst '${HMCR_DEPLOY_SUFFIX}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
